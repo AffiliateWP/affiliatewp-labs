ef3ea527b32faaf5e00e58cb8868f0a1085e2819
DrewAPicture <info@drewapicture.com>
2017-02-24 17:30:07 -0600

    Clean up logic for registering sections and controls, remove the panel registration, to be handled in core. Update docs.

--- includes/features/class-affiliate-area-styles.php
+++ includes/features/class-affiliate-area-styles.php
@@ -10,7 +10,7 @@ if ( ! class_exists( 'AffWP\Labs\Feature' ) ) {
 /**
  * Implements the Affiliate Area Styles feature for labs.
  *
- * @since 2.0.4
+ * @since 1.0
  *
  * @see \AffWP\Labs\Feature
  */
@@ -20,74 +20,73 @@ final class Affiliate_Area_Styles extends Labs\Feature {
 	 * Customizer instance.
 	 *
 	 * @access protected
-	 * @since  2.0.4
+	 * @since  1.0
 	 * @var    \WP_Customize_Manager
 	 */
 	protected $wp_customize;
 
 	/**
-	 * Affiliate Area panel ID.
+	 * Sets up the Affiliate Area Styles section in the Customizer.
 	 *
-	 * @access protected
-	 * @since  2.0.4
-	 * @var    string
+	 * @access public
+	 * @since  1.0
 	 */
-	protected $panel_id = 'affwp_labs_affarea_styles';
-
 	public function __construct() {
-		add_action( 'customize_register', array( $this, 'set_up_customizer' ) );
+		add_action( 'affwp_labs_customize_register', array( $this, 'set_up_customizer' ), 20 );
 
 		parent::__construct();
 	}
 
+	/**
+	 * Sets up the Customizer for AAS.
+	 *
+	 * @access public
+	 * @since  1.0
+	 *
+	 * @param \WP_Customize_Manager $wp_customize Customizer instance.
+	 */
 	public function set_up_customizer( $wp_customize ) {
 		$this->wp_customize = $wp_customize;
 
-		$this->register_panel();
 		$this->register_sections();
+		$this->register_controls();
 	}
 
-	public function register_panel() {
-		$this->wp_customize->add_panel( $this->panel_id, array(
-			// Deliberately not translatable.
-			'title' => 'AffiliateWP'
-		) );
-
-	}
 	/**
-	 * Registers the AMP Template panel sections.
+	 * Registers the the Affiliate Area Styles section.
 	 *
-	 * @since 0.4
 	 * @access public
+	 * @since  1.0
 	 */
 	public function register_sections() {
-		$this->wp_customize->add_section( 'affwp_etc_customizer_registration', array(
-			'title' => __( 'Registration', 'affiliate-wp' ),
-			'panel' => $this->panel_id
-		) );
-
-		$this->wp_customize->add_section( 'affwp_etc_customizer_accepted', array(
-			'title' => __( 'Accepted Affiliate', 'affiliate-wp' ),
-			'panel' => $this->panel_id
-		) );
-
-		$this->wp_customize->add_section( 'affwp_etc_customizer_pending', array(
-			'title' => __( 'Pending Affiliate', 'affiliate-wp' ),
-			'panel' => $this->panel_id
-		) );
-
-		$this->wp_customize->add_section( 'affwp_etc_customizer_referral', array(
-			'title' => __( 'New Referral', 'affiliate-wp' ),
-			'panel' => $this->panel_id
+		$this->wp_customize->add_section( 'affwp_labs_affiliate_area', array(
+			'title' => __( 'Affiliate Area Styles', 'affiliate-wp' ),
+			'panel' => affiliate_wp()->labs->get_panel_id()
 		) );
 	}
 
+	/**
+	 * Registers controls to the the Affiliate Area Styles section(s).
+	 *
+	 * @access public
+	 * @since  1.0
+	 */
+	public function register_controls() {
+		$this->wp_customize->add_control(
+			new \WP_Customize_Color_Control( $this->wp_customize, 'affiliate_area_primary', array(
+				'settings'   => 'affwp_settings',
+				'label'    => __( 'Primary Color', 'amp' ),
+				'section'  => 'affwp_labs_affiliate_area',
+				'priority' => 10
+			) )
+		);
+	}
 
 	/**
 	 * Registers the labs setting.
 	 *
 	 * @access public
-	 * @since  2.0.4
+	 * @since  1.0
 	 *
 	 * @param array $settings Labs settings.
 	 * @return array Modified labs settings.
