c03958f2754b90a4377eb1f18e1d5601db371f0d
DrewAPicture <info@drewapicture.com>
2017-02-26 17:00:55 -0700

    Switch to leveraging the new feature registry.

--- affiliatewp-labs.php
+++ affiliatewp-labs.php
@@ -239,14 +239,12 @@ if ( ! class_exists( 'AffiliateWP_Labs' ) ) {
 		 * @since  1.0
 		 */
 		private function register_features() {
-			affiliate_wp()->labs->register_feature( array(
-				'id'    => 'affiliate_area_styles',
+			affiliate_wp()->labs->registry->register_feature( 'affiliate_area_styles', array(
 				'class' => '\AffWP\Labs\Affiliate_Area_Styles',
 				'file'  => AFFWP_LABS_PLUGIN_DIR . 'includes/features/class-affiliate-area-styles.php'
 			) );
 
-			affiliate_wp()->labs->register_feature( array(
-				'id' => 'email_customizer',
+			affiliate_wp()->labs->registry->register_feature( 'email_customizer', array(
 				'class' => '\AffWP\Labs\Email_Customizer',
 				'file'  => AFFWP_LABS_PLUGIN_DIR . 'includes/features/class-email-customizer.php'
 			) );
